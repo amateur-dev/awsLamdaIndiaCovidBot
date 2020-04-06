c142df1a7e1bdfd6aeb8e9d2f9af4c452c0d04ac
Dipesh Sukhani <dipeshsukhani@capbridge.sg>
2020-04-07 00:40:41 +0800

    refactored code

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ const TGTOKEN = "1124930298:AAGdvQFjPAf2jG91NWrw6joFgwZkeKBFRrM"
 
 async function getCovidData() {
   var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
-  return JSON.stringify(reply.data.data.summary.total)
+  return JSON.stringify(reply.data.data)
 };
 
 async function sendToUser(chat_id, text) {
@@ -23,15 +23,16 @@ async function sendToUser(chat_id, text) {
 module.exports.covidbot = async event => {
   const body = JSON.parse(event.body);
   const { chat, text } = body.message;
-
-  let message = '';
-  try {
-    const result = await getCovidData();
-    message = `${body.text}`;
-  } catch (error) {
-    message = `Sorry, there was an error in securing data`;
+  let result = getCovidData();
+  let message;
+  if (text == "/getlatestdata") {
+    message = "The current total number of officially confirmed cases are "+ result.summary.total;
+  } else {
+    message = `Hi, Thank you for reaching out.  Please use the available commands to check the latest data.`;
   }
+
   await sendToUser(chat.id, message);
+
   return { statusCode: 200 };
 };
 
