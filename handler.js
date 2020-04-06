e02f463df960a3cc0bcc6cbd96628427ea72dacb
Dipesh Sukhani <dipeshsukhani@capbridge.sg>
2020-04-07 01:12:27 +0800

    working lambda

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ const TGTOKEN = "1124930298:AAGdvQFjPAf2jG91NWrw6joFgwZkeKBFRrM"
 
 async function getCovidData() {
   var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
-  return JSON.stringify(reply.data.data)
+  return JSON.stringify(reply.data.data.summary.total)
 };
 
 async function sendToUser(chat_id, text) {
@@ -22,14 +22,14 @@ async function sendToUser(chat_id, text) {
 
 module.exports.covidbot = async event => {
   const body = JSON.parse(event.body);
-  const { chat, text } = body.message;
-  let result = getCovidData();
-  let message;
-  if (text == "/getlatestdata") {
-    message = "The current total number of officially confirmed cases are "+ result.summary.total;
-  } else {
-    message = `Hi, Thank you for reaching out.  Please use the available commands to check the latest data.`;
-  }
+  const { chat } = body.message;
+  let message = await getCovidData();
+  // let message;
+  // if (text == "/getlatestdata") {
+  //   message = "The current total number of officially confirmed cases are "+ result.summary.total;
+  // } else {
+  //   message = `Hi, Thank you for reaching out.  Please use the available commands to check the latest data.`;
+  // }
 
   await sendToUser(chat.id, message);
 
